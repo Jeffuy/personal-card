8c0717520698baa347a89b7e15a190aa44ecd930
Jeff Free <jefffreelance3@gmail.com>
2022-09-04 03:35:32 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const $links = document.getElementById("links");
+const $name = documente.querySelector("h1");
+
+const data = {
+	name: "Germán Cavani",
+	nickname: "JeffUy",
+	description: "...",
+	avatar: "...",
+	social: [
+		{
+			name: "linkedin",
+			url: "https://linkedin.com/in/german-c-b0b371218/",
+			username: "German Cavani",
+		},
+		{
+			name: "instagram",
+			url: "https://instagram.com/kwanggae",
+			username: "kwanggae",
+		},
+	],
+	links: [
+		{
+			name: "KwangGae App",
+			url: "https://www.kwanggae.com/",
+			color: "red",
+			emoji: "📖",
+		},
+		{
+			name: "Website",
+			url: "https://www.gcavani.com/",
+			color: "yellow",
+			emoji: "💬",
+		},
+	],
+	footer: "Made with Love on Uruguay",
+};
