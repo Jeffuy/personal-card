292d0520962739f0c1dba81d55bf1c8f9fbe2b51
Jeff Free <jefffreelance3@gmail.com>
2022-09-05 22:27:18 -0300

    updated info

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const $links = document.getElementById("links");
-const $name = documente.querySelector("h1");
+const $name = document.querySelector("h1");
 
 const data = {
 	name: "Germán Cavani",
 	nickname: "JeffUy",
-	description: "...",
-	avatar: "...",
+	description:
+		"Web developer con conocimientos de HTML, CSS, Javascript, ReactJS, NextJS, Bootstrap, Git, Github, entre otros. En constante actualización y aprendizaje. Apasionado de las nuevas tecnologías.",
+	avatar: "https://i.imgur.com/uFBywHX.jpg",
 	social: [
 		{
 			name: "linkedin",
@@ -20,16 +21,16 @@ const data = {
 	],
 	links: [
 		{
+			name: "Personal Website",
+			url: "https://www.gcavani.com/",
+			color: "yellow",
+			emoji: "🌐",
+		},
+		{
 			name: "KwangGae App",
 			url: "https://www.kwanggae.com/",
 			color: "red",
-			emoji: "📖",
-		},
-		{
-			name: "Website",
-			url: "https://www.gcavani.com/",
-			color: "yellow",
-			emoji: "💬",
+			emoji: "🥋",
 		},
 	],
 	footer: "Made with Love on Uruguay",
@@ -37,15 +38,17 @@ const data = {
 
 const main = () => {
 	let name = document.createTextNode(data?.name);
-	let links = data?.links?.map((link) => {
-		return `<div class="bg-${link.color}-200 px-4 py-5 w-full flex justify-between">
+	let links = data?.links
+		?.map((link) => {
+			return `<div class="bg-${link.color}-200 px-4 py-5 w-full flex justify-between">
 		<a class="text-sm font-bold text-${link.color}-600 text-center hover:${link.color}-800 cursor-pointer"
 			href=${link.url} target="_blank">
 			${link.name}
 		</a>
 		<span>${link.emoji}</span>
 	</div>`;
-	}).join('');
+		})
+		.join("");
 
 	let newItem = document.createElement("section");
 	newItem.innerHTML = links;
@@ -53,4 +56,17 @@ const main = () => {
 	$name.appendChild(name);
 };
 
-main()
+const avatar = () => {
+	let $avatar = document.getElementById("avatar");
+	$avatar.innerHTML = `<img id="avatar" class="rounded-lg w-36" src=${data?.avatar} alt="German Cavani" />`;
+};
+
+const description = () => {
+	let $description = document.getElementById("description");
+	let description = document.createTextNode(data?.description);
+	$description.appendChild(description);
+};
+
+main();
+avatar();
+description();
